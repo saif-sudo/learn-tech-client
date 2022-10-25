c96a7b7107d8a8c59e092762911317867965cb44
saif-sudo <mdalisaif4@gmail.com>
2022-10-26 04:33:12 +0600

    login button icons added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,13 +1,15 @@
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { FaGoogle , FaGithub } from "react-icons/fa";
+import { AiOutlineMail } from "react-icons/ai";
 
 function Login() {
   return (
     <>
       <ButtonGroup size="lg" className="mb-2">
-        <Button>Email</Button>
-        <Button>Google</Button>
-        <Button>Github</Button>
+        <Button variant="outline-primary">Login With Email | <AiOutlineMail></AiOutlineMail>  </Button>
+        <Button variant="outline-warning">Login With Google | <FaGoogle></FaGoogle> </Button>
+        <Button variant="outline-dark">Login With Github | <FaGithub></FaGithub></Button>
       </ButtonGroup>
       <br />
      
