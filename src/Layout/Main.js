25e72e4f161e390e14722c57ecfb542c5005bae1
saif-sudo <mdalisaif4@gmail.com>
2022-10-26 00:51:42 +0600

    folder structure created

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import {Col, Container, Row } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
 
 const Main = () => {
     return (
-        <div>
-            <Outlet></Outlet>
-        </div>
+      <div>
+        <Container>
+
+         <Row>
+            <Col>
+                <h2>hello world</h2>
+            </Col>
+
+         </Row>
+
+        </Container>
+      </div>
     );
 };
 
---
+++ src/Pages/Shared/RightSideCat/RightSideCat.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RightSideCat = () => {
+    return (
+        <div>
+            <h2>Right side demo</h2>
+        </div>
+    );
+};
+
+export default RightSideCat;
